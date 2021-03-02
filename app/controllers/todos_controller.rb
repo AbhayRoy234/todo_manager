9b4e3cd0f55ed29d15b7cba1dbc683863336122f
Abhay Rai <royabhay063@gmail.com>
2021-03-02 23:53:29 +0530

    fixing databases errors on heroku

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,7 +1,8 @@
 # todos_controller.rb
 class TodosController < ApplicationController
   def index
-    render plain: Todo.order(:due_date).map { |todos| todos.to_pleasent_string }.join("\n")
+    render "index"
+    # render plain: Todo.order(:due_date).map { |todos| todos.to_pleasent_string }.join("\n")
   end
 
   # --- index is used to shiow the list of
@@ -13,7 +14,8 @@ class TodosController < ApplicationController
     id = params[:id]
     # render plain: "his is waht you typed id #{id}"
     todos = Todo.find(id)
-    render plain: todos.to_pleasent_string
+    render "todo"
+    # render plain: todos.to_pleasent_string
     # else
     # render plain: "you have exterd the "
     # end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,24 @@
+require "date"
+
 class Todo < ActiveRecord::Base
   def to_pleasent_string
     is_completed = completed ? "[x]" : "[]"
     "#{due_date.to_s(:long)}  #{id}  #{todo_text} #{is_completed} "
+  end
+
+  def self.due_today
+    all.where("due_date =?", Date.today)
+  end
+
+  def self.due_Overdue
+    all.where("due_date < ?", Date.today)
+  end
+
+  def self.due_Later
+    all.where("due_date > ?", Date.today)
+  end
+
+  def self.completed
+    all.where("completed = ?", true)
   end
 end
