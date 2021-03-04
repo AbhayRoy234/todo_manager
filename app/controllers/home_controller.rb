ee7899ed973ee9a4767bafbc5b0d05115eedb999
Abhay Rai <royabhay063@gmail.com>
2021-03-04 17:32:31 +0530

    Added css in SignIn And SignUp

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @current_user_id = session[:current_user_id]
     render "index"
     # render plain: Todo.order(:due_date).map { |todos| todos.to_pleasent_string }.join("\n")
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
+      session[:current_user_id] = user.id
       render plain: "You have eneter the correct password"
     else
       render plain: "opps ! Enter the correct password"
